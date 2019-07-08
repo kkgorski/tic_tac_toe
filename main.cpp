a069b74793abff16f20fa17f4189c6034efe9937
kkgorski <jgorski63@gmail.com>
2019-07-08 23:33:51 +0200

    Refactoring of main, making sure that check if some won is being executed after every move

--- main.cpp
+++ main.cpp
@@ -3,56 +3,89 @@
 #include <Solver/solver.hpp>
 #include <Combinations/combinations.hpp>
 
+bool checkIfSomeoneWon(Combinations& combinations)
+{
+	if(combinations.checkIfxWon())
+	{
+		std::cout << "!!! x won !!!" << std::endl;
+		return true;
+	}
+	if(combinations.checkIfoWon())
+	{
+		std::cout << "!!! o won !!!" << std::endl;
+		return true;
+	}
+	return false;
+}
+
+void makeUserMove(char character, Board &board)
+{
+	Point userPoint;
+	userPoint.character = character;
+	std::cout << "Give me coordinate x" << std::endl;
+	std::cin >> userPoint.x;
+	std::cout << "Give me coordinate y" << std::endl;
+	std::cin >> userPoint.y;
+
+	try
+	{
+		board.markField(userPoint);
+	}
+	catch(std::invalid_argument exception)
+	{
+		std::cerr << exception.what();
+	}
+
+	board.drawScreen();
+}
+
+void makeComputersMove(char character, Board& board)
+{
+	Combinations combinations = Combinations(board.getBoard());
+	Solver solver = Solver(combinations.getCombinationList());
+	Point solverPoint = solver.primitiveSolve();
+	solverPoint.character = character;
+
+	std::cout << "Computer's coordinates are x:" << solverPoint.x << " y: " << solverPoint.y << std::endl;
+	try
+	{
+	board.markField(solverPoint);
+	}
+	catch(std::invalid_argument exception)
+	{
+		std::cerr << exception.what();
+	}
+
+	board.drawScreen();
+}
 
 int main()
 {
 	Board board;
-	Point userPoint;
 
-	std::string userCharacter = " ";
+	std::string userInput= " ";
 	std::cout << "Please choose you character to be 'x' or 'o'" << std::endl;
 	do
 	{
-	        std::cin >> userCharacter;
+	        std::cin >> userInput;
 		std::cout << std::endl;
-	}while(userCharacter != "x" && userCharacter != "o");
-	userPoint.character = userCharacter.c_str()[0];
+	}while(userInput != "x" && userInput != "o");
+	char userCharacter = userInput.c_str()[0];
+	char solverCharacter = (userCharacter == 'x') ? 'o' : 'x';
 
 	while(true)
 	{
-	        std::cout << "Give me coordinate x" << std::endl;
-	        std::cin >> userPoint.x;
-	        std::cout << "Give me coordinate y" << std::endl;
-	        std::cin >> userPoint.y;
-
-		try
+		makeUserMove(userCharacter, board);
+		Combinations combinations1 = Combinations(board.getBoard());
+		if(checkIfSomeoneWon(combinations1))
 		{
-			board.markField(userPoint);
+			break;
 		}
-		catch(std::invalid_argument exception)
+		makeComputersMove(solverCharacter, board);
+		Combinations combinations2 = Combinations(board.getBoard());
+		if(checkIfSomeoneWon(combinations2))
 		{
-			std::cerr << exception.what();
-			continue;
-		}
-
-		board.drawScreen();
-
-		Combinations combinations = Combinations(board.getBoard());
-		Solver solver = Solver(combinations.getCombinationList());
-		Point solverPoint = solver.primitiveSolve();
-		solverPoint.character = (userPoint.character == 'x') ? 'o' : 'x';
-
-	        std::cout << "Computer's coordinates are x:" << solverPoint.x << " y: " << solverPoint.y << std::endl;
-		board.markField(solverPoint);
-		board.drawScreen();
-
-		if(combinations.checkIfxWon())
-		{
-			std::cout << "!!! x won !!!" << std::endl;
-		}
-		if(combinations.checkIfoWon())
-		{
-			std::cout << "!!! o won !!!" << std::endl;
+			break;
 		}
 	}
 }
